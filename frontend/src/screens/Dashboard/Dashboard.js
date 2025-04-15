27a4fe3b3f3578d874db9b6987e59d9d6151c256
Aaron Chacko <aaronchacko05@gmail.com>
2025-04-15 11:59:31 +0530

    About Us card n routing

--- frontend/src/screens/Dashboard/Dashboard.js
+++ frontend/src/screens/Dashboard/Dashboard.js
@@ -9,6 +9,7 @@ import PersonIcon from "@mui/icons-material/Person";
 import CountUp from "react-countup";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import InfoIcon from "@mui/icons-material/Info";
 
 function DashboardCard({ title, icon, count }) {
   return (
@@ -36,18 +37,20 @@ function DashboardCard({ title, icon, count }) {
         >
           {title}
         </Typography>
-        <Typography
-          variant="h4"
-          className="card-count"
-          sx={{
-            fontFamily: "Orbitron, sans-serif",
-            fontSize: 24,
-            fontWeight: 600,
-            color: "#FF6500",
-          }}
-        >
-          <CountUp end={parseInt(count)} duration={1.5} />
-        </Typography>
+        {count && (
+          <Typography
+            variant="h4"
+            className="card-count"
+            sx={{
+              fontFamily: "Orbitron, sans-serif",
+              fontSize: 24,
+              fontWeight: 600,
+              color: "#FF6500",
+            }}
+          >
+            <CountUp end={parseInt(count)} duration={1.5} />
+          </Typography>
+        )}
       </CardContent>
     </Card>
   );
@@ -65,12 +68,12 @@ const Dashboard = () => {
           fontSize: 38,
           fontWeight: "bold",
           textShadow: "0px 0px 5px rgba(255, 101, 0, 0.75)",
-          animation: "pulse 2s infinite",
           marginBottom: 8,
         }}
       >
         Device Inventory Dashboard
       </Typography>
+
       <Grid container spacing={4} justifyContent="space-evenly">
         <Grid
           item
@@ -93,11 +96,11 @@ const Dashboard = () => {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6, delay: 0.1 }}
             >
-            <DashboardCard
-              title="Total Devices"
-              icon={<DevicesIcon className="dashboard-icon" />}
-              count="132"
-            />
+              <DashboardCard
+                title="Total Devices"
+                icon={<DevicesIcon className="dashboard-icon" />}
+                count="132"
+              />
             </motion.div>
           </Link>
         </Grid>
@@ -118,16 +121,16 @@ const Dashboard = () => {
           }}
         >
           <Link to="/edit" style={{ textDecoration: "none" }}>
-          <motion.div
+            <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6, delay: 0.1 }}
             >
-            <DashboardCard
-              title="Add or Edit"
-              icon={<AddBoxIcon className="dashboard-icon" />}
-              count="+"
-            />
+              <DashboardCard
+                title="Add or Edit"
+                icon={<AddBoxIcon className="dashboard-icon" />}
+                count="+"
+              />
             </motion.div>
           </Link>
         </Grid>
@@ -148,16 +151,16 @@ const Dashboard = () => {
           }}
         >
           <Link to="/assign" style={{ textDecoration: "none" }}>
-          <motion.div
+            <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6, delay: 0.1 }}
             >
-            <DashboardCard
-              title="Assigned Devices"
-              icon={<AssignmentIndIcon className="dashboard-icon" />}
-              count="89"
-            />
+              <DashboardCard
+                title="Assigned Devices"
+                icon={<AssignmentIndIcon className="dashboard-icon" />}
+                count="89"
+              />
             </motion.div>
           </Link>
         </Grid>
@@ -178,16 +181,16 @@ const Dashboard = () => {
           }}
         >
           <Link to="/reports" style={{ textDecoration: "none" }}>
-          <motion.div
+            <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6, delay: 0.1 }}
             >
-            <DashboardCard
-              title="Reports"
-              icon={<ReportIcon className="dashboard-icon" />}
-              count="View"
-            />
+              <DashboardCard
+                title="Reports"
+                icon={<ReportIcon className="dashboard-icon" />}
+                count="View"
+              />
             </motion.div>
           </Link>
         </Grid>
@@ -208,19 +211,53 @@ const Dashboard = () => {
           }}
         >
           <Link to="/users" style={{ textDecoration: "none" }}>
-          <motion.div
+            <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6, delay: 0.1 }}
             >
-            <DashboardCard
-              title="Users"
-              icon={<PersonIcon className="dashboard-icon" />}
-              count="1"
-            />
+              <DashboardCard
+                title="Users"
+                icon={<PersonIcon className="dashboard-icon" />}
+                count="1"
+              />
             </motion.div>
           </Link>
         </Grid>
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        md={4}
+        sx={{
+          display: "flex",
+          justifyContent: "center", // Centers horizontally
+          alignItems: "center", // Centers vertically
+          transition: "transform 0.3s ease",
+          "&:hover": {
+            transform: "scale(1.03)", // Hover effect
+          },
+          position: "fixed", // Fix the position at the bottom of the screen
+          bottom: "20px", // Distance from the bottom of the screen
+          left: "50%", // Center horizontally
+          //transform: "translateX(-50%)", // Corrects the centering
+          marginBottom: "80px", // Adds bottom margin to prevent overlap with other elements
+        }}
+      >
+        <Link to="/profile" style={{ textDecoration: "none" }}>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.1 }}
+          >
+            <DashboardCard
+              title="About Us"
+              icon={<InfoIcon className="dashboard-icon" />}
+            />
+          </motion.div>
+        </Link>
       </Grid>
     </Box>
   );
