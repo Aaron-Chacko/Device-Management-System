73d384659e0a933ccf7c4e2b5277a0e247bad9c4
Aaron Chacko <aaronchacko05@gmail.com>
2025-04-27 19:33:23 +0530

    Update server.js
    added post requests for different routes

--- backend/server.js
+++ backend/server.js
@@ -38,5 +38,47 @@ app.post('/add-activity', (req, res) => {
   });
 });
 
+app.post('/add-device', (req, res) => {
+  const { deviceID, deviceName, routerSN, group, status, locationID, versionID, networkID, activityID } = req.body;
+
+  const query = "INSERT INTO devices (DeviceID, DeviceName, RouterSN, Group, Status, LocationID, VersionID, NetworkID, ActivityID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  db.query(query, [deviceID, deviceName, routerSN, group, status, locationID, versionID, networkID, activityID], (err, result) => {
+    if (err) {
+      console.error("Error inserting device:", err);
+      return res.status(500).json({ success: false });
+    }
+    return res.json({ success: true });
+  });
+});
+
+app.post('/add-network', (req, res) => {
+  const { networkID, networkType, networkLoopbackIP, networkLANIP, networkWANIP, networkLANStatus, networkWANStatus } = req.body;
+
+  const query = "INSERT INTO networks (NetworkID, NetworkType, LoopbackIP, LANIP, WANIP, LANStatus, WANStatus) VALUES (?, ?, ?, ?, ?, ?, ?)";
+  
+  db.query(query, [networkID, networkType, networkLoopbackIP, networkLANIP, networkWANIP, networkLANStatus, networkWANStatus], (err, result) => {
+    if (err) {
+      console.error("Error inserting network:", err);
+      return res.status(500).json({ success: false });
+    }
+    return res.json({ success: true });
+  });
+});
+
+app.post('/add-version', (req, res) => {
+  const { versionID, versionsoft, versionhard } = req.body;
+
+  const query = "INSERT INTO versions (VersionID, SoftVersion, HardVersion) VALUES (?, ?, ?)";
+  
+  db.query(query, [versionID, versionsoft, versionhard], (err, result) => {
+    if (err) {
+      console.error("Error inserting version:", err);
+      return res.status(500).json({ success: false });
+    }
+    return res.json({ success: true });
+  });
+});
+
+
 app.listen(5000, () => console.log("Server running on http://localhost:5000"));
 
